6708266770a3162dc6176b93348c53064351357e
ablossomhead <aimeecapps@hotmail.com>
2020-09-11 16:58:53 -0400

    deleted old database.

--- findfaceapi/server.js
+++ findfaceapi/server.js
@@ -27,34 +27,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const database = {
-    users: [
-        {
-            id:'123',
-            name:'John',
-            email:'john@gmail.com',
-            password:'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id:'124',
-            name:'Sally',
-            email:'sally@gmail.com',
-            password:'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
-app.get('/', (req, res)=> {res.send(database.users)})
+
+app.get('/', (req, res)=> {res.send('It is working!')})
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
